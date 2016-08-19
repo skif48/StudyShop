e0cd7997f1798e87fb1ee437473402225a657188
Vladyslav <vu4848@gmail.com>
2016-08-19 12:10:56 +0300

    added productType

---
+++ src/main/java/com/shop/entity/ProductType.java
@@ -0,0 +1,57 @@
+package com.shop.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+/**
+ * Created by Vladyslav Usenko on 18.08.2016.
+ */
+@Entity
+@Table(name = "productType")
+public class ProductType {
+    private long typeID;
+    private String name;
+    private Set<Product> productSet;
+
+    public ProductType() {
+    }
+
+    public ProductType(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @NotNull
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name= "increment", strategy= "increment")
+    @Column(name = "productType_id", length = 6, nullable = false)
+    public long getTypeID() {
+        return typeID;
+    }
+
+    public void setTypeID(long typeID) {
+        this.typeID = typeID;
+    }
+
+    @Column(name = "name")
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "productID", cascade = CascadeType.ALL)
+    public Set<Product> getProductSet() {
+        return productSet;
+    }
+
+    public void setProductSet(Set<Product> productSet) {
+        this.productSet = productSet;
+    }
+}
