7859060c475a16916f59689648e8571fe4389626
Vladyslav <vu4848@gmail.com>
2016-08-19 18:46:50 +0300

    simple authentification enabled

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,3 +1,14 @@
 function sendRequest(){
-    alert('hello!');
+    var login = document.getElementById("loginInput").value;
+    var password = document.getElementById("passwordInput").value;
+    var data = {};
+    data.login = login;
+    data.password = password;
+    $.ajax({
+        type: "POST",
+        url: "/",
+        processData: false,
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+    });
 }
